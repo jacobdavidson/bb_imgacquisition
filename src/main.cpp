9e31ae63c978f2f85d10e4b0fcfd89478142af34
Moritz Maxeiner <mm@ucw.sh>
2020-04-29 08:00:25 +1000

    remove non-portable handling of segfault

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-//
 #include "ImgAcquisitionApp.h"
 #include "settings/Settings.h"
 #include "settings/utility.h"
 
-#include <stdio.h>
-#include <execinfo.h>
-#include <signal.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-/*
- * If the segfault is caused from parsing config, well...
- * -> infinte loop.
- */
-int handlecounter = 0;
-
-/**
- * @brief handles segmentation faults
- */
-void handler(int sig)
-{
-    if (handlecounter > 0)
-        exit(1);
-    handlecounter++;
-
-    void*  array[10];
-    size_t size;
-
-    // get void*'s for all entries on the stack
-    size = backtrace(array, 10);
-
-    // print out all the frames to stderr
-    fprintf(stderr, "Error: signal %d:\n", sig);
-    backtrace_symbols_fd(array, size, STDERR_FILENO);
-
-    exit(1);
-}
-
 int main(int argc, char* argv[])
 {
-    signal(SIGSEGV, handler); // install exception handler
     SettingsIAC::setConf("configImAcq.json");
     SettingsIAC::getInstance();
 
