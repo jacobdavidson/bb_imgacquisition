d610da1f55ef1a05f0a5f5f8912c96a0832edaba
Moritz Maxeiner <mm@ucw.sh>
2020-05-20 22:23:21 +1000

    misc

--- src/cmake/BuildTimestamp.cmake
+++ src/cmake/BuildTimestamp.cmake
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-string(TIMESTAMP BUILD_TIMESTAMP "%Y-%m-%dT%H:%MZ" UTC)
+string(TIMESTAMP BUILD_TIMESTAMP "%Y-%m-%dT%H:%M:%SZ" UTC)
 
 configure_file("${SRC}" "${DST}" @ONLY)
