3b73efd0141e8a0044599471aa85249258b2465c
Moritz Maxeiner <mm@ucw.sh>
2020-05-21 00:11:19 +1000

    Add CSV-formatted logging to output file

--- src/log.cpp
+++ src/log.cpp
@@ -3,8 +3,12 @@
 #include "log.h"
 
 #include <mutex>
+#include <chrono>
+#include <fstream>
 
 #include <QDebug>
+#include <QStandardPaths>
+#include <QDir>
 
 namespace log_detail
 {
@@ -53,5 +57,36 @@ static void customMessageHandler(QtMsgType                 type,
                                  const QString&            msg)
 {
     auto lock = std::unique_lock(log_detail::mutex());
+
+    {
+        const auto timestamp = std::chrono::system_clock::now();
+
+        const auto dataLocation = QDir(
+            QStandardPaths::writableLocation(QStandardPaths::AppLocalDataLocation));
+        if (!dataLocation.exists())
+        {
+            dataLocation.mkpath(".");
+        }
+
+        try
+        {
+            const auto formattedMsg = fmt::format("{:e.3},{},{:q}\n", timestamp, type, msg);
+
+            auto logFile = std::fstream(dataLocation.absoluteFilePath("log.csv").toStdString(),
+                                        std::ios::out | std::ios::app);
+            logFile.write(formattedMsg.c_str(), formattedMsg.size());
+            logFile.close();
+        }
+        catch (const std::exception& e)
+        {
+            (*g_defaultMessageHandler)(QtWarningMsg,
+                                       QMessageLogContext{QT_MESSAGELOG_FILE,
+                                                          QT_MESSAGELOG_LINE,
+                                                          QT_MESSAGELOG_FUNC,
+                                                          "default"},
+                                       e.what());
+        }
+    }
+
     (*g_defaultMessageHandler)(type, context, msg);
 }
