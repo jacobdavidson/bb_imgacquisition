55b6a407253e41474278950c6a8f1aeb2129c72c
Moritz Maxeiner <mm@ucw.sh>
2020-06-02 16:56:55 +0200

    Ensure that camera roi <= camera parameter roi

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -277,6 +277,16 @@ Settings::Settings()
             stream.encoder.options.emplace(key, value.get_value<std::string>());
         }
 
+        if (!(stream.camera.offset_x + stream.camera.width == stream.camera.params.width))
+        {
+            throw std::runtime_error(fmt::format("{}: Camera offset_x + Camera width != Camera parameter width", stream.id));
+        }
+
+        if (!(stream.camera.offset_y + stream.camera.height == stream.camera.params.height))
+        {
+            throw std::runtime_error(fmt::format("{}: Camera offset_y + Camera height != Camera parameter height", stream.id));
+        }
+
         _streams.push_back(stream);
     }
 
