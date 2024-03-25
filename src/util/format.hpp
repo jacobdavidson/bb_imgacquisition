67cf7e80349c983537d96463056af21b8c86c8bf
jacobdavidson <jddavid@umich.edu>
2024-03-25 08:19:22 +0100

    updated format.hpp for compatibillity with libfmt v8
    Added explicit specialization for system clock with nanoseconds.  This was not needed for Ubuntu 20 / libfmt 6, but without this the time stamp output was not working with Ubuntu 22 / libfmt 8

--- src/util/format.hpp
+++ src/util/format.hpp
@@ -16,90 +16,133 @@
 #include <optional>
 #include <cmath>
 
-template<typename Clock, typename Duration, typename Char>
-struct fmt::formatter<std::chrono::time_point<Clock, Duration>, Char>
-{
-    using time_point = std::chrono::time_point<Clock, Duration>;
 
-    // Presentation format: 'b' - basic, 'e' - extended.
-    char presentation = 'b';
 
-    // Fractional second precision
+namespace fmt {
+
+struct base_time_formatter {
+    char presentation = 'b'; // Default presentation ('b' for basic, 'e' for extended)
     std::optional<std::size_t> precision;
 
     // Parses format specifications of the form ['b' | 'e'] ['.' ('1' - '9')].
-    constexpr auto parse(fmt::format_parse_context& ctx)
-    {
-        auto       it  = ctx.begin();
-        const auto end = ctx.end();
-
-        if (it != end && (*it == 'b' || *it == 'e'))
-        {
+    constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.begin()) {
+        auto it = ctx.begin(), end = ctx.end();
+        if (it != end && (*it == 'b' || *it == 'e')) {
             presentation = *it++;
         }
-        if (it != end && *it == '.')
-        {
+        if (it != end && *it == '.') {
             ++it;
-            if (it != end && '1' <= *it && *it <= '9')
-            {
-                precision = static_cast<unsigned int>(*it++ - '0');
-            }
-            else
-            {
+            if (it != end && '1' <= *it && *it <= '9') {
+                precision = static_cast<std::size_t>(*it++ - '0');
+            } else {
                 throw format_error("invalid format");
             }
         }
-
-        if (it != end && *it != '}')
-        {
+        if (it != end && *it != '}') {
             throw format_error("invalid format");
         }
-
         return it;
     }
 
-    template<typename FormatContext>
-    auto format(const time_point& tp, FormatContext& ctx)
-    {
-        // NOTE: std::gmtime is not thread safe, fmt::gmtime is
-        const auto tm = fmt::gmtime(Clock::to_time_t(tp));
+    // Utility for formatting time_point to a string with specified presentation and precision
+    template <typename Clock, typename Duration, typename FormatContext>
+    auto format_time_point(const std::chrono::time_point<Clock, Duration>& tp, FormatContext& ctx) -> decltype(ctx.out()) {
+        std::time_t tt = Clock::to_time_t(tp);
+        std::tm* tm = std::gmtime(&tt); // Note: std::gmtime is not thread-safe
 
-        if (precision)
-        {
-            const auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(
-                                tp.time_since_epoch())
-                                .count() %
-                            1000000000;
-            const decltype(ns) tr = std::trunc(static_cast<double>(ns) /
-                                               (std::pow(10, 9 - *precision)));
+        // Handling fractional seconds precision if specified
+        auto nanos = std::chrono::duration_cast<std::chrono::nanoseconds>(tp.time_since_epoch()).count() % 1000000000;
+        std::string fractional_seconds = precision ? fmt::format(".{:0{}}", nanos / std::pow(10, 9 - *precision), *precision) : "";
 
-            if (presentation == 'b')
-            {
-                return format_to(ctx.out(), "{:%Y%m%dT%H%M%S}.{:0{}}Z", tm, tr, *precision);
-            }
-            else if (presentation == 'e')
-            {
-                return format_to(ctx.out(), "{:%Y-%m-%dT%H:%M:%S}.{:0{}}Z", tm, tr, *precision);
-            }
+        if (presentation == 'b') {
+            return fmt::format_to(ctx.out(), "{:%Y%m%dT%H%M%S}{}Z", *tm, fractional_seconds);
+        } else {
+            return fmt::format_to(ctx.out(), "{:%Y-%m-%dT%H:%M:%S}{}Z", *tm, fractional_seconds);
         }
-        else
-        {
-            if (presentation == 'b')
-            {
-                return format_to(ctx.out(), "{:%Y%m%dT%H%M%S}Z", tm);
-            }
-            else if (presentation == 'e')
-            {
-                return format_to(ctx.out(), "{:%Y-%m-%dT%H:%M:%S}Z", tm);
-            }
-        }
-
-        throw std::logic_error("");
     }
 };
 
+// Generic formatter
+// Should apply to :
+// std::chrono::system_clock
+// std::chrono::steady_clock
+// std::chrono::nanoseconds
+// std::chrono::microseconds
+// std::chrono::seconds
+// but see below for additions to ensure functionality with libfmt v8
+template<typename Clock, typename Duration, typename Char>
+struct formatter<std::chrono::time_point<Clock, Duration>, Char> : base_time_formatter {
+    using time_point = std::chrono::time_point<Clock, Duration>;
+
+    constexpr auto parse(format_parse_context& ctx) {
+        return base_time_formatter::parse(ctx);
+    }
+
+    template<typename FormatContext>
+    auto format(const time_point& tp, FormatContext& ctx) {
+        return base_time_formatter::format_time_point(tp, ctx);
+    }
+};
+
+// Specialization for different types, to ensure they are recognized (added due to problems with libfmt v8)
+// system_clock with nanoseconds needed to be treated differently
+
+// Specialization for std::chrono::system_clock with std::chrono::nanoseconds
+template<>
+struct formatter<std::chrono::time_point<std::chrono::system_clock, std::chrono::nanoseconds>, char> : base_time_formatter {
+    constexpr auto parse(format_parse_context& ctx) {
+        return base_time_formatter::parse(ctx);
+    }
+
+    template<typename FormatContext>
+    auto format(const std::chrono::time_point<std::chrono::system_clock, std::chrono::nanoseconds>& tp, FormatContext& ctx) {
+        return base_time_formatter::format_time_point(tp, ctx);
+    }
+};
+
+// system_clock with microseconds
+template<>
+struct formatter<std::chrono::time_point<std::chrono::system_clock, std::chrono::microseconds>, char>
+    : public formatter<std::chrono::time_point<std::chrono::system_clock, std::chrono::microseconds>::clock, 
+                       std::chrono::time_point<std::chrono::system_clock, std::chrono::microseconds>::duration, char> {
+    // Inherits everything from the base formatter
+};
+
+// system_clock with seconds
+template<>
+struct formatter<std::chrono::time_point<std::chrono::system_clock, std::chrono::seconds>, char>
+    : public formatter<std::chrono::time_point<std::chrono::system_clock, std::chrono::seconds>::clock, 
+                       std::chrono::time_point<std::chrono::system_clock, std::chrono::seconds>::duration, char> {
+    // Inherits everything from the base formatter
+};
+
+// steady_clock with nanoseconds
+template<>
+struct formatter<std::chrono::time_point<std::chrono::steady_clock, std::chrono::nanoseconds>, char>
+    : public formatter<std::chrono::time_point<std::chrono::steady_clock, std::chrono::nanoseconds>::clock, 
+                       std::chrono::time_point<std::chrono::steady_clock, std::chrono::nanoseconds>::duration, char> {
+    // Inherits everything from the base formatter
+};
+
+
+// steady_clock with microseconds
+template<>
+struct formatter<std::chrono::time_point<std::chrono::steady_clock, std::chrono::microseconds>, char>
+    : public formatter<std::chrono::time_point<std::chrono::steady_clock, std::chrono::microseconds>::clock, 
+                       std::chrono::time_point<std::chrono::steady_clock, std::chrono::microseconds>::duration, char> {
+    // Inherits everything from the base formatter
+};
+
+// steady_clock with seconds
+template<>
+struct formatter<std::chrono::time_point<std::chrono::steady_clock, std::chrono::seconds>, char>
+    : public formatter<std::chrono::time_point<std::chrono::steady_clock, std::chrono::seconds>::clock, 
+                       std::chrono::time_point<std::chrono::steady_clock, std::chrono::seconds>::duration, char> {
+    // Inherits everything from the base formatter
+};
+
 template<typename Char>
-struct fmt::formatter<std::thread::id, Char>
+struct formatter<std::thread::id, Char>
 {
     constexpr auto parse(fmt::format_parse_context& ctx)
     {
@@ -124,7 +167,7 @@ struct fmt::formatter<std::thread::id, Char>
 };
 
 template<typename Char>
-struct fmt::formatter<boost::filesystem::path, Char>
+struct formatter<boost::filesystem::path, Char>
 {
     constexpr auto parse(fmt::format_parse_context& ctx)
     {
@@ -147,7 +190,7 @@ struct fmt::formatter<boost::filesystem::path, Char>
 };
 
 template<typename Char>
-struct fmt::formatter<QtMsgType, Char>
+struct formatter<QtMsgType, Char>
 {
     constexpr auto parse(fmt::format_parse_context& ctx)
     {
@@ -184,7 +227,7 @@ struct fmt::formatter<QtMsgType, Char>
 };
 
 template<typename Char>
-struct fmt::formatter<QString, Char>
+struct formatter<QString, Char>
 {
     bool quoted = false;
 
@@ -222,3 +265,5 @@ struct fmt::formatter<QString, Char>
         }
     }
 };
+
+} // end using namespace fmt
--- src/util/log.cpp
+++ src/util/log.cpp
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #include "log.hpp"
+#include "format.hpp"
 
 #include <mutex>
 #include <chrono>
