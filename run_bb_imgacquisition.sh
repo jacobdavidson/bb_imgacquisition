1d8f85b340b9f8d5a2033df872114789f9e18d0b
jacobdavidson <jddavid@umich.edu>
2024-07-09 15:53:28 +0200

    Update run_bb_imgacquisition.sh

--- run_bb_imgacquisition.sh
+++ run_bb_imgacquisition.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # run in an infinite loop with highest priority, so that it restarts after a crash
 
+cd build
+
 while true
 do
-	nice -n -20 ./build/bb_imgacquisition
-	date >> build/crashreport_bb_imgacquisition.txt
+	nice -n -20 ./bb_imgacquisition
+	date >> crashreport_bb_imgacquisition.txt
 done
